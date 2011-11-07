6db585bf4a358ee360683be042839fd6b1fe57c4
Thomas ZILLIOX <thomas@zilliox.me>
2011-11-07 22:56:30 +0100

    Add go logic : dertimine in which group is a stone

--- Goban.js
+++ Goban.js
@@ -36,7 +36,7 @@ var Goban = $.inherit( Canvas, {
 			turn.color = ( turns[ turns.length - 1 ].color + 1 ) % 2;
 		}
 		turns[ turns.length ] = turn;
-		stones = this.add_stone( stones, x, y, turn.color );
+		stones = this.play_stone( stones, turn );
 		this.set({
 			turns: turns,
 			stones: stones,
@@ -47,12 +47,95 @@ var Goban = $.inherit( Canvas, {
         /***
            PRIVATE METHODS
         ***/
-	add_stone: function( stones, x, y, color ) {
-		if ( typeof stones[ x ] == "undefined" ) {	
-			stones[ x ] = [];
-		} 
-		stones[ x ][ y ] = color;
+	add_stone: function( stones, turn ) {
+		if ( this.is_coord_on_goban( turn ) ) { 
+			if ( typeof stones[ turn.x ] == "undefined" ) {	
+				stones[ turn.x ] = [];
+			} 
+			stones[ turn.x ][ turn.y ] = turn.color;
+		}
 		return stones;
+	},
+	get_stone: function ( stones, coord ) {
+		if ( typeof stones[ coord.x ] == "undefined" ||	
+			typeof stones[ coord.x ][ coord.y ] == "undefined"
+		) {
+			return -1;
+                }
+		return stones[ coord.x ][ coord.y ];
+	},
+	play_stone: function( stones, turn ) {
+		stones = this.add_stone( stones, turn );
+		var coord_around = this.coord_around( turn );
+		for ( var i in coord_around ) {
+			if ( this.get_stone( stones, coord_around[ i ] ) == ( turn.color + 1 ) % 2 &&
+			     this.stone_liberties( stones, coord_around[ i ] ) == 0 
+			) {
+				stones = this.remove_stone_group( stones, turn );
+			}
+                }
+                //if ( this.stone_liberties( stones, turn ) == 0 ) {
+                //       	alert( 'suicide' ); 
+                //}
+		return stones;
+	},
+	stone_liberties: function( stones, coord ) {
+		var group = this.get_stone_group( stones, coord );
+		console.dir( group );
+		return 1;
+	},
+	remove_stone_group: function( stones, coord ) {
+		alert( 'dead: ' +coord.x+','+coord.y );
+		return stones;
+        },
+	get_stone_group: function( stones, coord, coord_from, group ) {
+		if ( typeof coord_from == "undefined" ) {
+			coord_from = { x: -2, y: -2 };
+		}
+		if ( typeof group == "undefined" ) {
+			group = [ ];
+		}
+		var found = false;
+		for ( var i in group ) {
+			if ( group[ i ].x == coord.x && group[ i ].y == coord.y ) {
+				found = true;
+				break;
+			}
+		}
+		if ( found == false ) {
+			group[ group.length ] = coord;
+			var coord_around = this.coord_around( coord );
+			for ( var i in coord_around ) {
+	                        if (    ! ( coord_around[ i ].x == coord_from.x && coord_around[ i ].y == coord_from.y ) && 
+					this.get_stone( stones, coord_around[ i ] ) == this.get_stone( stones, coord ) 
+	                        ) {
+	                                group = this.get_stone_group( stones, coord_around[ i ], coord, group );
+	                        }
+        	        }
+		}
+		return group; 
+	},
+	coord_around: function( coord ) {
+		var coords = [
+			{ x: coord.x - 1, y: coord.y },
+			{ x: coord.x, y: coord.y - 1 },
+			{ x: coord.x + 1, y: coord.y },
+			{ x: coord.x, y: coord.y + 1 }
+		];
+		var coord_around = [];
+		for ( var i in coords ) {
+			if ( this.is_coord_on_goban( coords[ i ] ) ) {
+				coord_around[ coord_around.length ] = coords[ i ]
+			}
+		}
+		return coord_around;
+	},
+	is_coord_on_goban: function( coord ) {
+		return ( coord.x > 0 &&
+                	coord.y > 0 &&
+                	coord.x <= this.get( 'goban_size' ) &&
+                	coord.y <= this.get( 'goban_size' )
+                );
 	},
     
 
